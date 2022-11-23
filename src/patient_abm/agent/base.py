c74911630d223b6ab5a5acdc37db9f30a068304c
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-23 09:33:48 +0000

    Adding tarfile member sanitization to extractall()

--- src/patient_abm/agent/base.py
+++ src/patient_abm/agent/base.py
@@ -106,7 +106,29 @@ class Agent:
             temp_dir = Path(temp_dir)
 
             with tarfile.open(tar_file, "r") as archive:
-                archive.extractall(path=str(temp_dir))
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(archive, path=str(temp_dir))
 
             for attr_name in cls.serialisable_attributes:
 
